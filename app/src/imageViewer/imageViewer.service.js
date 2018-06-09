0978b9fb1e48bd16ef8450708afa3416605bee57
angelodel80 <angelodel80@gmail.com>
2018-06-09 11:16:36 +0200

    stating work for feature/lexo

--- app/src/imageViewer/imageViewer.service.js
+++ app/src/imageViewer/imageViewer.service.js
@@ -9,7 +9,11 @@
         var options = {
             id: "osd_img",
             prefixUrl: "bower_components/openseadragon/built-openseadragon/openseadragon/images/",
-            tileSources: "data/tails/scaled_70_verticale.dzi", // immagine per rotulo Alba
+            tileSources:{
+                type:'image',
+                url: 'data/test-img/saussure-page-84.jpg', // immagine per saussure
+            }, 
+            //tileSources: "data/tails/scaled_70_verticale.dzi", // immagine per rotulo Alba
             //tileSources: "data/test-img/quamusdzi/QuamusImgManuscript.dzi", // immagine per Marocco
             //overlays: [{
             //    id: 'example-overlay',
@@ -33,7 +37,7 @@
     
             //sequenceMode: true,
             //degrees:90,
-            toolbar:"toolbar-div",
+            //toolbar:"toolbar-div",
             showRotationControl: true,                
             showNavigator: true,
             //navigatorId: "navscroll",
@@ -47,15 +51,15 @@
             maxZoomLevel: 8.0,
     
             wrapVertical: false,
-            navigatorPosition: "ABSOLUTE",
+            //navigatorPosition: "ABSOLUTE",
             //navigatorTop: "0",
             //navigatorLeft: "94%",
             //navigatorHeight: "100%",
             //navigatorWidth: "6%",
-            navigatorTop:"1%",
-            navigatorLeft:"94%",
-            navigatorHeight:"95%",
-            navigatorWidth:"6%"
+            //navigatorTop:"1%",
+            //navigatorLeft:"94%",
+            //navigatorHeight:"95%",
+            //navigatorWidth:"6%"
          };
          // {id:"box_body_mainImage",prefixUrl:"images/",tileSources:"data/tails/scaled_70_verticale.dzi",showNavigator:!0,visibilityRatio:1,defaultZoomLevel:1,panHorizontal:!0,constrainDuringPan:!0,minZoomLevel:1,wrapVertical:!0,navigatorPosition:"ABSOLUTE",navigatorTop:"1%",navigatorLeft:"98%",navigatorHeight:"95%",navigatorWidth:"8%"}
     
